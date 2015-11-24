b2c63aeb0e611974179592cda5085a357e27af88
Fabíola <fabi.tuany@gmail.com>
2015-11-24 20:29:13 -0200

    Inserção de e-mail na tabela - Listas

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -1,10 +1,14 @@
 <?php
 
+function gerarCodigo(){
+    return sha1(mt_rand());
+}
+
 /* * Verifica se o botão cadastrar foi pressionado
  * 
  */
 if (isset($_POST['btn'])) {
-    
+
     //Faz a requisição de dados para a conexão com o BD
     require_once 'dbconfig.php';
 
@@ -18,11 +22,31 @@ if (isset($_POST['btn'])) {
         die("Não foi possível se conectar ao banco $dbname :" . $pe->getMessage());
     }
 
+
     /**
      * Recepção de dados
      */
-    echo "<h1> $_POST[email] </h1>";
-
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        //Filtragem de entrada de dados
+        
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $cod = gerarCodigo();
+        
+        //String SQL
+        $sql = "INSERT INTO lista(email, cod, dtCadastro) values(:email,:cod,now())";
+        $parametros = array(':email'=>$email,':cod'=>$cod);
+        $p = $conn->prepare($sql);
+        $q = $p->execute($parametros);
+        
+        /**Tarefa de casa
+         * Criar um e-mail HTML, enviando um link com o código, 
+         * para a pessoa clicar e confirmar seu e-mail
+         */
+        
+        
+    } else {
+        header('Location: index.php');
+    }
 
 
 //Fecha conexão com o banco
